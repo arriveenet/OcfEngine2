d7c6de0fdb803661d94f8af3b6feb98fa3c1ef93
Tsuyoshi KATAYAMA <katayama933@gmail.com>
2025-10-20 23:31:19 +0900

    Variant型にPackedUint32ArrayとPackedVec4Arrayを追加
    `PackedUint32Array`と`PackedVec4Array`型を追加し、`Variant`型および`VariantType`列挙型でこれらをサポートするように変更しました。また、`<cstdint>`と`ocf/math/vec4.h`をインクルードして必要な型を利用可能にしました。

--- engine/include/ocf/core/Variant.h
+++ engine/include/ocf/core/Variant.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "ocf/math/vec2.h"
 #include "ocf/math/vec3.h"
+#include "ocf/math/vec4.h"
+#include <cstdint>
 #include <string>
 #include <variant>
 #include <vector>
@@ -8,8 +10,10 @@
 namespace ocf {
 
 using NoneType = std::monostate;
+using PackedUint32Array = std::vector<uint32_t>;
 using PackedVec2Array = std::vector<math::vec2>;
 using PackedVec3Array = std::vector<math::vec3>;
+using PackedVec4Array = std::vector<math::vec4>;
 
 enum class VariantType {
     None = 0,
@@ -17,11 +21,13 @@ enum class VariantType {
     Int,
     Float,
     String,
-
+    PackedUint32Array,
     PackedVec2Array,
     PackedVec3Array,
+    PackedVec4Array,
 };
 
-using Variant = std::variant<NoneType, bool, int, float, std::string, PackedVec2Array, PackedVec3Array>;
+using Variant = std::variant<NoneType, bool, int, float, std::string,
+                             PackedUint32Array, PackedVec2Array, PackedVec3Array, PackedVec4Array>;
 
 } // namespace ocf
